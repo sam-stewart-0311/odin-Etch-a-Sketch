8b91507d1cb11678bd93e255a736dc77feb747c7
sam-stewart-0311 <149884032+sam-stewart-0311@users.noreply.github.com.>
2024-05-05 18:03:04 +0100

    Adding functionality so set grid size using JS

---
+++ index.js
@@ -0,0 +1,7 @@
+// *************** Set Grid Size ***************
+const gridContainer = document.querySelector("#grid-container");
+
+let gridHeight = 200;
+let gridWidth = 200;
+gridContainer.style.height = `${gridHeight}px`;
+gridContainer.style.width = `${gridWidth}px`;
