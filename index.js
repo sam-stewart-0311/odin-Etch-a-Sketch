8a0812cfe23062eeb4757639a558b9f5013b9c7c
sam-stewart-0311 <149884032+sam-stewart-0311@users.noreply.github.com.>
2024-05-05 19:59:00 +0100

    Added Basic Functionality(Fill grid and add Colour)

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
 // *************** Set Grid Size ***************
+
 const gridContainer = document.querySelector("#grid-container");
 
-let gridHeight = 200;
-let gridWidth = 200;
+let gridHeight = 700;
+let gridWidth = 700;
 gridContainer.style.height = `${gridHeight}px`;
 gridContainer.style.width = `${gridWidth}px`;
+
+// *************** Add Grid Items ***************
+
+let gridResolution = 60;
+
+document.addEventListener('DOMContentLoaded', populateGrid);
+
+function populateGrid() {
+  for (let i = 0; i < (gridResolution ** 2); i++) {
+    const gridItem = document.createElement("div");
+  
+    gridItem.className = "grid-item";
+    gridItem.style.height = `${gridHeight / gridResolution}px`;
+    gridItem.style.width = `${gridWidth / gridResolution}px`;
+
+    gridItem.addEventListener("mouseenter", () => changeColour(gridItem));
+  
+    gridContainer.appendChild(gridItem);
+  };
+};
+
+function changeColour(gridItem) {
+  gridItem.style.backgroundColor = "black";
+}
+
+
+
