186a27f7ee27e09dc98a02258eb47f04124d476d
sam-stewart-0311 <149884032+sam-stewart-0311@users.noreply.github.com.>
2024-05-06 09:16:49 +0100

    Added functionality to update grid resolution

--- index.js
+++ index.js
@@ -2,18 +2,22 @@
 
 const gridContainer = document.querySelector("#grid-container");
 
-let gridHeight = 700;
-let gridWidth = 700;
+let gridHeight = 600;
+let gridWidth = 600;
 gridContainer.style.height = `${gridHeight}px`;
 gridContainer.style.width = `${gridWidth}px`;
 
 // *************** Add Grid Items ***************
 
-let gridResolution = 60;
+let gridResolution = 16;
 
 document.addEventListener('DOMContentLoaded', populateGrid);
 
 function populateGrid() {
+
+  // Clear Grid Before Populating
+  gridContainer.innerHTML = "";
+
   for (let i = 0; i < (gridResolution ** 2); i++) {
     const gridItem = document.createElement("div");
   
@@ -31,5 +35,21 @@ function changeColour(gridItem) {
   gridItem.style.backgroundColor = "black";
 }
 
+// *************** Update Grid ***************
 
+const gridResInput = document.querySelector("#grid-range");
+const gridXReadout = document.querySelector("#grid-x");
+const gridYReadout = document.querySelector("#grid-y");
+const updateGridBtn = document.querySelector("#update-grid-btn");
+
+gridResInput.addEventListener("input", () => {
+  let inputValue = gridResInput.value;
+  let inputValueString = inputValue.toString();
+
+  gridXReadout.textContent = inputValueString;
+  gridYReadout.textContent = inputValueString;
+  gridResolution = inputValue;
+});
+
+updateGridBtn.addEventListener("click", populateGrid);
 
